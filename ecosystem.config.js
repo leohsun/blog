b3b21ac4037f89a39b18a6259575cf013f880ed5
leohsun <leohsun@qq.com>
2018-05-06 11:32:06 +0800

    change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // First application
     {
       name      : 'Blog-server',
-      script    : './server/index.js',
+      script    : './build/server/index.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
@@ -30,7 +30,7 @@ module.exports = {
       repo : 'git@gitee.com:leohsun/blog.git',
       path : '/home/www/blog/production',
       "ssh_options": "StrictHostKeyChecking=no",
-      'post-deploy' : 'npm install --registry https://registry.npm.taobao.org && npm run build pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy' : 'npm install --registry https://registry.npm.taobao.org && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
       env : {
         NODE_ENV :'production'
       }
