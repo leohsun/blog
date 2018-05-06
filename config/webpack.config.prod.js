dd989281a7086b28acd9429f43f8624f73351ed6
leohsun <leohsun@qq.com>
2018-05-06 17:03:19 +0800

    change something

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,6 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
               compact: true,
             },
           },
