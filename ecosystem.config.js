0617333eac5e78b07ec7e6270e28b73843d9370d
leohsun <leohsun@qq.com>
2018-05-09 21:22:03 +0800

    deploy cfg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
       repo: 'git@gitee.com:leohsun/blog.git',
       path: '/home/www/blog/production',
       "ssh_options": "StrictHostKeyChecking=no",
-      "pre-deploy": "git pull",
+      "pre-deploy": "git fetch && git merge origin/master",
       'post-deploy': 'npm install --registry https://registry.npm.taobao.org && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
       env: {
         NODE_ENV: 'production'
